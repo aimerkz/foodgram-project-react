3afaf6d995ea10c0f43198214c22f3dad4e29c10
amerkulov <amerkulov@aorti.ru>
2022-07-15 10:59:10 +0300

    doc users viewset, fix gitignore

--- backend/foodgram_project/users/views.py
+++ backend/foodgram_project/users/views.py
@@ -3,5 +3,12 @@ from djoser.views import UserViewSet
 
 
 class CustomUserViewSet(UserViewSet):
-    """Вьюсэт Юзер"""
+    """Вьюсэт Юзер
+    Получение списка юзеров / 
+    конкретного юзера / 
+    создание, обновление / 
+    удаление конкретного юзера /
+    изменение пароля /
+    получение текущего юзера
+    """
     pagination_class = CustomPagination
