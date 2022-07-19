656ee90a87c2b273554c52a263e29793a2366326
amerkulov <amerkulov@aorti.ru>
2022-07-19 13:43:59 +0300

    add update method recipes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -122,3 +122,17 @@ class RecipeSerializer(serializers.ModelSerializer):
         recipes.tags.set(tags)
         self.create_ingredients(ingredients_data, recipes)
         return recipes
+
+    def update(self, instance, validated_data):
+        """Метод для обновления рецепта"""
+        instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get('cooking_time', instance.cooking_time)
+        instance.tags.clear()
+        tags_data = self.initial_data.get('tags')
+        instance.tags.set(tags_data)
+        IngredientRecipes.objects.filter(recipes=instance).all().delete()
+        self.create_ingredients(validated_data.get('ingredients'), instance)
+        instance.save()
+        return instance
