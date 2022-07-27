4201a3389559eca9dc89ae226c95b0e864e9d386
amerkulov <amerkulov@aorti.ru>
2022-07-27 11:29:54 +0300

    update env

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='1234s'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432'),
     }
